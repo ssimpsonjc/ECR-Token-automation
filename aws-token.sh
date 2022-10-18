4b2ebcc0e49e11e4df32fee3a55afea9402e970e
Steve Simpson <107069565+ssimpsonjc@users.noreply.github.com>
2022-10-18 07:50:17 -0400

    Use TOKEN definition that works

--- aws-token.sh
+++ aws-token.sh
@@ -1,9 +1,9 @@
 ACCOUNT=$(aws sts get-caller-identity --query 'Account' --output text) #aws account number
-REGION=ap-south-1                                     #aws ECR region
+REGION=us-west-2                                     #aws ECR region
 SECRET_NAME=${REGION}-ecr-registry                    #secret_name
 EMAIL=abc@xyz.com                                     #can be anything
 
-TOKEN=`aws ecr --region=$REGION get-authorization-token --output text --query authorizationData[].authorizationToken | base64 -d | cut -d: -f2`
+TOKEN=`aws ecr --region=$REGION get-authorization-token --output text --query authorizationData | cut -f1 | base64 -d | cut -d: -f2`
 
 kubectl delete secret --ignore-not-found $SECRET_NAME
 kubectl create secret docker-registry $SECRET_NAME \
